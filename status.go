d5a6a3f569aff3ed7844fc40400273e1f2504838
James Rhoat <james.rhoat@dbally.com>
2022-05-29 04:51:33 -0400

    updating status check

--- status.go
+++ status.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func (b *board) getGameStatus(turnNumber, player int) (gameOver bool) {
-	if b.checkforTie(turnNumber) {
-		fmt.Println("Game has tied")
+	if b.checkforWin() {
+		fmt.Printf("Player %d has Won!\n", player)
 		b.printBoard()
 		return true
 	}
-	if b.checkforWin() {
-		fmt.Printf("Player %d has Won!\n", player)
+	if b.checkforTie(turnNumber) {
+		fmt.Println("Game has tied")
 		b.printBoard()
 		return true
 	}

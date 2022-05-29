b43b3d67b90dff29d2f2d96589eb034befab5690
James Rhoat <james.rhoat@dbally.com>
2022-05-29 04:57:28 -0400

    updating winner checks to dynamically update checks based on size of the board

--- winner.go
+++ winner.go
@@ -1,37 +1,72 @@
 package main
 
+import (
+	"fmt"
+)
+
 func (b *board) checkforWin() (won bool) {
-	sums := make([]int, 8)
-	//0 | 1 | 2
-	//---------
-	//3 | 4 | 5
-	//---------
-	//6 | 7 | 8
-	//---------
-	//get column totals
-	sums[0] = b.playable_field[0] + b.playable_field[3] + b.playable_field[6]
-	sums[1] = b.playable_field[1] + b.playable_field[4] + b.playable_field[7]
-	sums[2] = b.playable_field[2] + b.playable_field[5] + b.playable_field[8]
-	//get row totals
-	sums[3] = b.playable_field[0] + b.playable_field[1] + b.playable_field[2]
-	sums[4] = b.playable_field[3] + b.playable_field[4] + b.playable_field[5]
-	sums[5] = b.playable_field[6] + b.playable_field[7] + b.playable_field[8]
-	//get diag totals
-	sums[6] = b.playable_field[0] + b.playable_field[4] + b.playable_field[8]
-	sums[7] = b.playable_field[2] + b.playable_field[4] + b.playable_field[6]
 
-	for _, v := range sums {
-		if v == player_O*3 {
-			return true
-		}
-		if v == player_x*3 {
-			return true
-		}
+	row := b.calcStraightWinner()
+	diag := b.calcDiagWinner()
 
+	if row || diag {
+		return true
 	}
 	return false
 }
 
 func (b *board) checkforTie(turn int) bool {
-	return turn == 9
+	return turn == b.size*b.size
+}
+
+func (b *board) calcStraightWinner() (winner bool) {
+	var totalRow, totalColumn int
+	for rowNumber := 0; rowNumber < b.size; rowNumber++ {
+
+		for columnNumber, _ := range make([]int, b.size) {
+			totalRow += b.playable_field[columnNumber+(rowNumber*b.size)]
+			totalColumn += b.playable_field[rowNumber+(columnNumber*b.size)]
+		}
+		if totalRow == b.winningTotalO || totalRow == b.winningTotalX || totalColumn == b.winningTotalO || totalColumn == b.winningTotalX {
+			return true
+		}
+		totalRow, totalColumn = 0, 0
+
+	}
+	return false
+}
+
+func (b *board) calcDiagWinner() (winner bool) {
+	var totalDownToRight, totalDownToLeft []int
+	index := 0
+	for row := 0; row < b.size; row++ {
+		for column := 0; column < b.size; column++ {
+			fmt.Printf("Column: %d Row: %d Index: %d \n", column, row, index)
+			if (row*b.size)+row == index {
+				totalDownToRight = append(totalDownToRight, b.playable_field[index])
+				fmt.Println("adding to totalDownToRight")
+			}
+			if (row+1)*b.size-1*(row+1) == index {
+				fmt.Println("adding to totalDownToLeft")
+				totalDownToLeft = append(totalDownToLeft, b.playable_field[index])
+			}
+			index++
+		}
+	}
+
+	fmt.Printf("totalDownToRight: %d totalDownToLeft: %d \n", totalDownToRight, totalDownToLeft)
+	if sumSlice(totalDownToLeft) == b.winningTotalO || sumSlice(totalDownToLeft) == b.winningTotalX {
+		return true
+	}
+	if sumSlice(totalDownToRight) == b.winningTotalO || sumSlice(totalDownToRight) == b.winningTotalX {
+		return true
+	}
+	return false
+}
+
+func sumSlice(slice []int) (result int) {
+	for _, v := range slice {
+		result += v
+	}
+	return result
 }

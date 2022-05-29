b58a089353ba38317d138a530e4f6376e5680639
James Rhoat <james.rhoat@dbally.com>
2022-05-29 04:56:44 -0400

    updating the print function to use the appropriate size value and player values

--- print.go
+++ print.go
@@ -12,15 +12,15 @@ func (b *board) printBoard() {
 	for i, v := range b.playable_field {
 		if v == 0 {
 			fmt.Printf("%d", i)
-		} else if v == player_x {
+		} else if v == b.player_x {
 			fmt.Print(color.Ize(color.Green, "X"))
-		} else if v == player_O {
+		} else if v == b.player_O {
 			fmt.Print(color.Ize(color.Yellow, "O"))
 		}
 		// Print the values in columns and rows
-		if i > 0 && (i+1)%b.rows == 0 {
+		if i > 0 && (i+1)%b.size == 0 {
 			fmt.Printf("\n")
-			fmt.Println(strings.Repeat("-", len(rowdivider)*b.rows))
+			fmt.Println(strings.Repeat("-", len(rowdivider)*b.size))
 		} else {
 			fmt.Print(rowdivider)
 		}

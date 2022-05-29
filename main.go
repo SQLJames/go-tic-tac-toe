9b5455e7fcd44ede5ecebc364ed5b1d6a7b4dc8e
James Rhoat <james.rhoat@dbally.com>
2022-05-29 04:53:02 -0400

    updating main file to simplify computations on game run time

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 var (
-	columns, rows int = 3, 3
-	player_x          = 10
-	player_O          = 100
+	size int = 3
 )
 
 func main() {
 	gameOver := false
 	turnNumber := 1
-
-	board := newBoard(columns, rows)
+	board := newBoard(size)
 	for !gameOver {
 		board.printBoard()
 		player := getPlayer(turnNumber)
 		board.executeMove(player)
-		gameOver = board.getGameStatus(turnNumber, player)
+		if turnNumber >= 5 {
+			gameOver = board.getGameStatus(turnNumber, player)
+		}
 		turnNumber++
-
 	}
 
 }
